576713c69d8ad15c7021796a1298a08e98e880c0
riamehra1 <riamehra2004@gmail.com>
2024-06-20 21:47:31 +0530

    Add files via upload

---
+++ TASK3.py
@@ -0,0 +1,15 @@
+import time
+import sys
+
+
+def countdown_timer(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+
+    print("00:00\nTime's up!")
+
+countdown_timer(10)
